281f8e9eb1a7b0ac338287f64db83b94df12cf58
wonjun <jwj51720@gmail.com>
2022-12-23 04:53:27 +0000

    [Feat] newMF.py 추가 - model/newMF.py 추가 - 프로젝트 기본 모델로 작동

---
+++ model/newMF.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class MatrixFactorization(torch.nn.Module):
+    def __init__(self, n_users, n_items, n_factors=20):
+        super().__init__()
+        self.user_factors = torch.nn.Embedding(n_users, n_factors, sparse=True)
+        self.item_factors = torch.nn.Embedding(n_items, n_factors, sparse=True)
+
+        self.item_factors.weight.data.uniform_(-1, 1) # uniform distribution weight init
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, user, items):
+        item_emb =self.item_factors(items[0])
+        for i in range(1, len(items)):
+            item_emb = item_emb*self.item_factors(items[i])
+        return self.sigmoid(item_emb.sum(0))
