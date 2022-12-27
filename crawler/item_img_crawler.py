9d553cc7580bb98ed70aeacb0e2802407dd8b780
RYU-BB <tmxkdd5790@gmail.com>
2022-12-27 14:46:33 +0900

    [Fix]: 아이템 이미지 크롤러, 유저 이미지 크롤러 수정
    - user img crawler 코드가 item img crawler에 입력된 현상 수정
    - Intent

--- crawler/item_img_crawler.py
+++ crawler/item_img_crawler.py
@@ -1,23 +1,13 @@
-import os
-import datetime
 import pandas as pd
+import os
 from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    base_folder = "../data/user_detail"
-    base_img_folder = "../data/user_img"
-    os.makedirs(base_img_folder, exist_ok=True)
-    for path in os.listdir(base_folder):
-        file_path = os.path.join(base_folder, path)
-        img_folder = os.path.join(base_img_folder, path[:-4])
-        os.makedirs(img_folder, exist_ok=True)
-        df = pd.read_csv(file_path)
-        cur_date = sorted(df["last_access"], reverse=True)[0].replace("/", "_")
+    df = pd.read_csv("data/maple_item.csv")
+    save_folder = "data/maple_item_img"
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
 
-        for idx in tqdm(range(len(df))):
-            user_detail = df.iloc[idx]
-            os.system("curl " + user_detail["cur_chr"] + f" >{img_folder}/user_{user_detail['nickname']}_{cur_date}.png")
-            for i in range(1, 7):
-                date = user_detail[f'past_chr_date_{i}'].replace("/", "_")
-                os.system("curl " + user_detail[f"past_chr_img_{i}"] + f" > {img_folder}/user_{user_detail['nickname']}_{date}.png")
+    for id, url in tqdm(zip(df["id"].values, df["imgUrl"].values)):
+        os.system("curl " + url + f" > data/maple_item_img/{id}.png")
