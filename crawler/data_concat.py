42c53d98636b42a6292e0a46574248fa054f5554
41ow1ives <77601251@daum.net>
2023-01-10 02:21:56 +0000

    [Update]: Add argparser to set path
    - add argparser to set user_info and user_detail path
    - created function making dataframe

--- crawler/data_concat.py
+++ crawler/data_concat.py
@@ -1,33 +1,71 @@
 import pandas as pd
 import os
+import argparse
+
+
+def making_df(base_path, base_df):
+    for path in os.listdir(base_path):
+        temp = pd.read_csv(os.path.join(base_path, path))
+        user_df = pd.concat([base_df, temp])
+    return user_df
 
 
 if __name__ == "__main__":
-    user_info_df = pd.DataFrame(columns=["server", "user_info_url", "user"])
+    parser = argparse.ArgumentParser()
+    arg = parser.add_argument
+    arg("--user_info_path", type=str, default="user_info")
+    arg("--user_detail_path", type=str, default="user_detail")
+    arg("--save_path", type=str, default="./")
+    args = parser.parse_args()
 
-    base_path = "user_info"
-    for user_info_path in os.listdir(base_path):
-        temp = pd.read_csv(os.path.join(base_path, user_info_path))
-        user_info_df = pd.concat([user_info_df, temp])
-
+    base_user_info = pd.DataFrame(columns=["server", "user_info_url", "user"])
+    user_info_df = making_df(args.user_info_path, base_user_info)
     user_info_df.columns = ["server", "user_info_url", "nickname"]
 
-    user_detail_df = pd.DataFrame(columns=['nickname', 'codi-hat', 'codi-hair', 'codi-face', 'codi-top',
-                                           'codi-bottom', 'codi-shoes', 'codi-weapon', 'level', 'class',
-                                           'popularity', 'total_ranking', 'world_ranking', 'class_world_ranking',
-                                           'class_total_ranking', 'guild', 'last_access', 'mureung', 'theseed',
-                                           'union', 'achievement', 'cur_chr', 'past_chr_img_1', 'past_chr_img_2',
-                                           'past_chr_img_3', 'past_chr_img_4', 'past_chr_img_5', 'past_chr_img_6',
-                                           'past_chr_date_1', 'past_chr_date_2', 'past_chr_date_3',
-                                           'past_chr_date_4', 'past_chr_date_5', 'past_chr_date_6'])
+    base_user_detail = pd.DataFrame(
+        columns=[
+            'nickname',
+            'codi-hat',
+            'codi-hair',
+            'codi-face',
+            'codi-top',
+            'codi-bottom',
+            'codi-shoes',
+            'codi-weapon',
+            'level',
+            'class',
+            'popularity',
+            'total_ranking',
+            'world_ranking',
+            'class_world_ranking',
+            'class_total_ranking',
+            'guild',
+            'last_access',
+            'mureung',
+            'theseed',
+            'union',
+            'achievement',
+            'cur_chr',
+            'past_chr_img_1',
+            'past_chr_img_2',
+            'past_chr_img_3',
+            'past_chr_img_4',
+            'past_chr_img_5',
+            'past_chr_img_6',
+            'past_chr_date_1',
+            'past_chr_date_2',
+            'past_chr_date_3',
+            'past_chr_date_4',
+            'past_chr_date_5',
+            'past_chr_date_6',
+        ]
+    )
 
-    base_path = "user_detail"
-    for user_detail_path in os.listdir(base_path):
-        temp = pd.read_csv(os.path.join(base_path, user_detail_path))
-        user_detail_df = pd.concat([user_detail_df, temp])
-
+    user_detail_df = making_df(args.user_detail_path, base_user_detail)
     user_detail_df = user_detail_df.drop_duplicates(subset=["nickname"])
 
-    user_df = pd.merge(left=user_detail_df, right=user_info_df, on="nickname", how="left")
+    user_df = pd.merge(
+        left=user_detail_df, right=user_info_df, on="nickname", how="left"
+    )
 
-    user_df.to_csv("user_data.csv", index=False)
+    user_df.to_csv(os.path.join(args.save_path, "user_data.csv"), index=False)
