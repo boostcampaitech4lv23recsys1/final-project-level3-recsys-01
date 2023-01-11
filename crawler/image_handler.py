2bd2b4a80a1a22a6b027a058c8fc4e0d9511e655
41ow1ives <77601251@daum.net>
2023-01-11 06:24:08 +0000

    [Fix]: available to set path as arg
    - Available to set path with argparser

--- crawler/image_handler.py
+++ crawler/image_handler.py
@@ -47,29 +47,31 @@ def drop_past_add_column(user_detail_info: pd.DataFrame) -> pd.DataFrame:
 
 
 def image_uploader(
-    user_detail_info: pd.DataFrame, gcs_helper: GCS_helper, csv_folder: str
+    user_detail_info: pd.DataFrame,
+    gcs_helper: GCS_helper,
+    img_folder: str,
+    csv_folder: str,
 ) -> None:
     local_images = user_detail_info['gcs_image_path'].apply(
         lambda x: f"user_{x.split('/')[3]}"
     )
     gcs_path = user_detail_info['gcs_image_path']
     for image, path in tqdm(zip(local_images, gcs_path)):
-        gcs_helper.upload_file_to_gcs(
-            path, os.path.join('./data/user_img', csv_folder, image)
-        )
+        gcs_helper.upload_file_to_gcs(path, os.path.join(img_folder, csv_folder, image))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-    arg('--base_folder', type=str, default='./data/user_detail')
+    arg('--detail_folder', type=str, default='./data/user_detail')
+    arg('--img_folder', type=str, default='./data/user_img')
     arg('--key_path', type=str, default='./config/key.json')
     args = parser.parse_args()
     gcs_helper = GCS_helper(args.key_path)
-    for csv_file in os.listdir(args.base_folder):
+    for csv_file in os.listdir(args.detail_folder):
         if not csv_file.endswith(".csv"):
             continue
-        csv_path = os.path.join(args.base_folder, csv_file)
+        csv_path = os.path.join(args.detail_folder, csv_file)
 
         data = pd.read_csv(csv_path)
         try:
@@ -78,4 +80,4 @@ if __name__ == "__main__":
         except:
             pass
 
-        image_uploader(data, gcs_helper, csv_path.split('/')[-1][:-4])
+        image_uploader(data, gcs_helper, args.img_folder, csv_path.split('/')[-1][:-4])
