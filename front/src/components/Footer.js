2315339a2bc56d07958bb7d64b55c1a77a8ae494
jwj51720 <jwj51720@gmail.com>
2023-01-24 22:54:30 +0900

    [Feat] Footer 구현 - 화면 바닥에 고정 - 배경 및 글자 색 및 글꼴 적용

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,39 +1,26 @@
-// import AppBar from "@mui/material/AppBar"; // 페이지 꽉차게 bar 생성
-// import Toolbar from "@mui/material/Toolbar"; // 글씨를 벽에서 떼 줌, 요소끼리 잘 붙여줌
-// import Typography from "@mui/material/Typography";
-// import { ThemeProvider, createTheme } from "@mui/material/styles"; // them 추가 및 생성
+import React from "react";
+import Box from "@mui/material/Box";
 
-function Footer() {
-  const footerdescription =
-    "Contact | gongryongal1@gmil.com \n 네이버 부스트캠프 AI Tech 4기 RecSys Track 공룡알 팀의 최종 프로젝트입니다.";
-  return <div className="footer">{footerdescription}</div>;
+export default function StickyFooter() {
+  const teamDescription =
+    "Contact | gongryongal1@gmail.com \n 네이버 부스트캠프 AI Tech 4기 RecSys Track 공룡알 팀의 최종 프로젝트입니다.";
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}>
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: "auto",
+          backgroundColor: "#FCCEAD",
+        }}>
+        <div className="text-footer">{teamDescription}</div>
+      </Box>
+    </Box>
+  );
 }
-
-export default Footer;
-
-// const footerTheme = createTheme({
-//   palette: {
-//     primary: {
-//       main: "#FCCEAD",
-//     },
-//   },
-//   typography: {
-//     fontFamily: ["NanumSquareAceb", "sans-serif"].join(","),
-//   },
-// });
-
-// function Footer() {
-//   return (
-//     <div className="wrap-container">
-//       <ThemeProvider theme={footerTheme}>
-//         <AppBar position="static" color="primary">
-//           <Toolbar variant="dense">
-//             <Typography variant="h6" color="white" component="div">
-//               메이플스토리 코디 추천
-//             </Typography>
-//           </Toolbar>
-//         </AppBar>
-//       </ThemeProvider>
-//     </div>
-//   );
-// }
