b86b8f2b779f59b65133829da9a8e7124b0ea69a
Jeong-Junhwan <zxxzx1515@naver.com>
2023-01-26 10:45:48 +0900

    [Update]: newMF 학습 방식 변경
    - 기존 코드는 착용하지 않은 아이템도 하나의 벡터로 학습
    - -1로 마스킹 된 아이템의 경우 고려하지 않도록 코드 변경
    - 다만 for loop을 도는 과정에서 학습 시간이 너무 많이 소요됨. 해결 방법은 주석으로 기제함.

--- modeling/model/newMF.py
+++ modeling/model/newMF.py
@@ -2,20 +2,35 @@ import torch
 import torch.nn as nn
 from typing import List
 
-class NewMF(torch.nn.Module):
-    def __init__(self, n_users: int, n_items: int, n_factors: int) -> None:
+
+class NewMF(nn.Module):
+    def __init__(self, n_items: int, n_factors: int) -> None:
         super().__init__()
 
-        self.user_factors = torch.nn.Embedding(n_users, n_factors, sparse=True)
-        self.item_factors = torch.nn.Embedding(n_items, n_factors, sparse=True)
+        self.item_factors = nn.Embedding(n_items, n_factors, sparse=True)
 
         # self.item_factors.weight.data.uniform_(-1, 1) # uniform distribution weight init
         self.sigmoid = nn.Sigmoid()
-    
-    def forward(self, items: List[int]) -> torch.tensor:
-        item_emb = self.item_factors(items[0])
-        for i in range(1, len(items)):
-            item_emb = item_emb * self.item_factors(items[i])
-            
-        return self.sigmoid(item_emb.sum(-1))
-    
+
+    def forward(self, items: torch.Tensor) -> torch.Tensor:
+
+        # 배치마다 학습에 사용하지 않는 부분이 달라서 그냥 일단 for loop 돌림
+        # 나중에 조금 더 고민해봐야 할 것 같다. 혹은 MCN 모델에는 이미 구현이 되어 있겠지?
+        """
+        이렇게 하면 masking 된 부분을 1로 바꿀 수 있다.
+        착용하지 않은 아이템을 -1로 해놨는데, 0으로 바꿔야 사용 가능.
+        -1이면 self.item_factors(items) 할 때 음수가 들어가서 오류난다.
+        mask = items != 0
+        vector = self.item_factors(items)
+        vector[~mask] = 1
+        """
+        inner_product = []
+
+        for items_per_batch in items:
+            items_per_batch = items_per_batch[items_per_batch != -1]
+            item_emb = self.item_factors(items_per_batch)
+            inner_product.append(torch.sum(torch.prod(item_emb, dim=0), dim=0))
+
+        inner_product = torch.stack(inner_product, dim=0)
+
+        return self.sigmoid(inner_product)
