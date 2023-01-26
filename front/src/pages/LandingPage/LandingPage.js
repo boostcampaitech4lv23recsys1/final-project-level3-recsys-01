0c2a71a45856626b40d365c792faa073969e11b7
jwj51720 <jwj51720@gmail.com>
2023-01-26 20:51:38 +0900

    [Fix]: LandingPage 요청 사항 수정 - LandingPage에 컴포넌트 3개만 표시되도록 변경 - 변수 및 컴포넌트의 이름 수정 - 컴포넌트 분리 및 추가 - navigate 추가해 페이지 내 이동 - member.js 삭제 - center tag 삭제해 text-align 추가 - Component.js 삭제 - img 태그에 alt 추가

--- front/src/pages/LandingPage/LandingPage.js
+++ front/src/pages/LandingPage/LandingPage.js
@@ -1,55 +1,14 @@
 import "./LandingPage.css";
-
-import TitleDescription from "../../components/TitleDescription";
-import RoundSquare from "./components/RoundSquare";
-import LinkButton from "./components/LinkButton";
-import dinoEgg from "../../assets/icons/dino_egg_white.png";
-import github from "../../assets/icons/github.png";
+import TitleDescription from "./components/TitleDescription";
+import TeamDescription from "./components/TeamDescription";
+import ChoicePreferOrConcept from "./components/ChoicePreferOrConcept";
 
 function LandingPage() {
-  const titleOne = "Home";
-  const titleOneDes =
-    "이 기능에 대해 뭐라고\n 한 두 줄 정도로 예쁘고 간결하게 설명하기";
-  const preferDes =
-    "부위별 코디 추천을 설명 \n 위 아이콘은 부위별 코디추천 아이콘";
-  const preferMoveDes = "코디 추천 받기";
-  const titleTwo = "Teams";
-  const titleTwoDes = "어떤 가치를 어쩌구\n 저쩌구\n 호롤롤루";
-  const githubLink = "https://www.naver.com/";
-  const linkname = "Team Github";
-
   return (
-    <div>
-      <TitleDescription
-        title={titleOne}
-        description={titleOneDes}></TitleDescription>
-      <br></br>
-      <center>
-        <div className="body-one-margin">
-          <RoundSquare
-            icon={dinoEgg}
-            description={preferDes}
-            movedescription={preferMoveDes}></RoundSquare>
-        </div>
-        <div className="body-one-margin">
-          <RoundSquare
-            icon={dinoEgg}
-            description={preferDes}
-            movedescription={preferMoveDes}></RoundSquare>
-        </div>
-      </center>
-      <TitleDescription
-        title={titleTwo}
-        description={titleTwoDes}></TitleDescription>
-      <center>
-        <div className="body-two-margin">
-          <img src={github} width="50px" height="50px" className="img-margin" />
-        </div>
-        <LinkButton
-          link={githubLink}
-          displaylink={linkname}
-          className="body-two-margin"></LinkButton>
-      </center>
+    <div className="LandingPage">
+      <TitleDescription></TitleDescription>
+      <ChoicePreferOrConcept></ChoicePreferOrConcept>
+      <TeamDescription></TeamDescription>
     </div>
   );
 }
--- front/src/pages/LandingPage/components/Member.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Member({ img, name, intro }) {
-  return (
-    <div>
-      <img src={img} width="50px" height="50px" className="img-member"></img>
-    </div>
-  );
-}
