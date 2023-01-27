cdcf0be4664f2720d67f89579193634773f1464b
Jeong-Junhwan <zxxzx1515@naver.com>
2023-01-27 17:58:41 +0900

    [Feat]: MCN dataset 초안 완성
    - image는 tensor로 반환
    - 착용하지 않은 부위에 대한 이미지는, 해당 부위의 평균 이미지를 사용할 예정
    - 추후에 혹시 있을 negative sampling을 위해 y 값 일단 반환
    - resize 할 때 크기의 경우 일단 MCN 논문을 참고하여 224로 결정, 추후에 변경 될 여지 충분히 있음

--- modeling/dataset/MCN_dataset.py
+++ modeling/dataset/MCN_dataset.py
@@ -1,14 +1,40 @@
+import torch
+import torchvision
 from torch.utils.data import Dataset
+
+from PIL import Image
+
+from pandas import DataFrame
+from typing import Dict, Union
 
 
 class MCNDataset(Dataset):
-    def __init__(self, data) -> None:
+    def __init__(self, inter_data: DataFrame) -> None:
         super().__init__()
-        pass
+        self.inter_data = inter_data
+        self.y = [1] * len(inter_data)
 
     def __len__(self) -> int:
-        pass
+        return len(self.inter_data)
 
-    def __getitem__(self, index):
+    def __getitem__(self, index) -> Dict[str, Union[torch.Tensor, int]]:
+        images = []
 
-        pass
+        for image_path in self.inter_data.iloc[index]:
+            image = Image.open(image_path).convert("RGB")
+            image = self.transform()(image)
+            images.append(image)
+
+        images = torch.stack(images)
+
+        return {"x": images, "y": self.y[index]}
+
+    def transform(self) -> torchvision.transforms.Compose:
+        img_size = 224
+        trans = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.Resize(img_size, img_size),
+                torchvision.transforms.ToTensor(),
+            ]
+        )
+        return trans
