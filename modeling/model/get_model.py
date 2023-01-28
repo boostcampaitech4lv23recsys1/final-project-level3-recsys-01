2d73f8d9c2b2f442b155883d9d59df64f621cdd8
ryubright <tmxkdd5790@gmail.com>
2023-01-28 21:28:25 +0900

    [Fix]: MCN 학습 기본 셋팅
    - MCN_config 설정 필요 설정 추가 (먼지 까먹금)
    - train에 get_trainers 추가

--- modeling/model/get_model.py
+++ modeling/model/get_model.py
@@ -10,6 +10,17 @@ def get_models(config: Dict[str, Any]) -> nn.Module:
         model = getattr(models, config["arch"]["type"])(
             model_config["n_items"], model_config["n_factors"]
         )
+    elif config["arch"]["type"] == "MCN":
+        model_config = config["arch"]["args"]
+        model = getattr(models, config["arch"]["type"])(
+            embed_size=1000,
+            need_rep=True,
+            vocabulary=config["dataloader"]["args"]["vocabulary_len"],
+            vse_off=model_config["vse_off"],
+            pe_off=model_config["pe_off"],
+            mlp_layers=model_config["mlp_layers"],
+            conv_feats=model_config["conv_feats"],
+        )
     else:
         raise NotImplementedError
     return model
--- modeling/trainer/__init__.py
+++ modeling/trainer/__init__.py
@@ -1,1 +1,2 @@
 from .newMF_trainer import newMFTrainer
+from .MCN_trainer import MCNTrainer
