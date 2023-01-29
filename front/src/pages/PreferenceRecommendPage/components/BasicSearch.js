f2d7ce5c8a99ff4e6708ccaf99196e487c9b3fbd
41ow1ives <77601251@daum.net>
2023-01-29 18:20:08 +0900

    [Update]: Available to show image and data
    - connected with backend
    - enable to show image and name of items
    - still need to update the key-value of ItemLabel

--- front/src/pages/PreferenceRecommendPage/components/BasicSearch.js
+++ front/src/pages/PreferenceRecommendPage/components/BasicSearch.js
@@ -1,23 +1,62 @@
 import * as React from "react";
+import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import { codiPartToData } from "./ItemLabel";
+import { codiPartToCategory } from "./ItemLabel";
+import * as API from "../../../api";
+import { render } from "react-dom";
+
+function handleInputChange(value) {
+  console.log(value);
+}
 
 function BasicSearch(codiPart) {
+  const [codiPartData, setCodiPartData] = React.useState([]);
+  React.useEffect(() => {
+    const getCodiPartData = async () => {
+      const codiPartKey = Object.values(codiPart)[0];
+      const codiPartCategory = codiPartToCategory[codiPartKey];
+      if (!codiPart || !codiPartCategory) return;
+      const res = await API.get(`items/${codiPartCategory}`);
+      const data = res.data.items;
+      const codiPartData = [];
+      for (let currentItem of Object.values(data)) {
+        codiPartData.push({
+          label: currentItem["name"],
+          img: currentItem["gcs_image_url"],
+        });
+      }
+      setCodiPartData(codiPartData);
+    };
+    getCodiPartData();
+  }, [codiPart, codiPartToCategory]);
   const codiPartKey = Object.values(codiPart)[0];
-  const codiPartData = codiPartToData[codiPartKey];
   return (
-    <Autocomplete
-      id="popover-searchbox"
-      options={codiPartData}
-      sx={{ width: "300px" }}
-      ListboxProps={{
-        style: {
-          maxHeight: "125px",
-        },
-      }}
-      renderInput={(params) => <TextField {...params} label={codiPartKey} />}
-    />
+    <>
+      <Autocomplete
+        id="popover-searchbox"
+        options={codiPartData}
+        sx={{ width: "300px" }}
+        autoHighlight
+        onInputChange={handleInputChange}
+        renderOption={(props, codiPartData) => (
+          <Box
+            component="li"
+            sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+            {...props}>
+            <img
+              loading="lazy"
+              width="20"
+              src={codiPartData.img}
+              srcSet={`${codiPartData.img} 2x`}
+              alt={codiPartData.label}
+            />
+            {codiPartData.label}
+          </Box>
+        )}
+        renderInput={(params) => <TextField {...params} label={codiPartKey} />}
+      />
+    </>
   );
 }
 
