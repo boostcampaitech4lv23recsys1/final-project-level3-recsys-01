6d48bc9a8d2abb4f8e4a4144093e29f936287280
jwj51720 <jwj51720@gmail.com>
2023-02-03 00:21:56 +0900

    [Feat] Result Page 백과 연결
    - 작업 중, 백업용

--- front/src/pages/PreferenceRecommendResultPage/PreferenceRecommendResultPage.js
+++ front/src/pages/PreferenceRecommendResultPage/PreferenceRecommendResultPage.js
@@ -3,8 +3,16 @@ import BestCodiTopThree from "./components/BsetCodiTopThree";
 import RetryButton from "./components/RetryButton";
 import "./PreferenceRecommendResultPage.css";
 
-function PreferenceRecommendResultPage() {
-  const fixPartList = ["헤어", "상의", "하의", "신발", "무기"];
+function PreferenceRecommendResultPage(props) {
+  const fixPartList = [];
+  for (let idx = 0; idx < props.length; idx++) {
+    if (props[idx]) {
+      fixPartList.push(props[idx]);
+    } else {
+      continue;
+    }
+  }
+  // const fixPartList = ["헤어", "상의", "하의", "신발", "무기"];
   return (
     <div className="PRRP">
       <TitleFixItem fixPartList={fixPartList}></TitleFixItem>
