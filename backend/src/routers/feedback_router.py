545d02944041558a330fb6ba2acc0c170b48f1b8
Jeong-Junhwan <zxxzx1515@naver.com>
2023-02-03 16:42:41 +0900

    [Feat]: 유저 피드백 결과 저장
    - BigQueryHelper에 json 저장 코드 추가
    - log dataset, Feedback table에 피드백 정보 저장
    - FeedbackSchema 변수 명 수정 필요

--- backend/src/routers/feedback_router.py
+++ backend/src/routers/feedback_router.py
@@ -5,12 +5,15 @@ from typing import Dict
 
 from src.database.schemas import FeedbackSchema
 from src.database.models.Feedbacks import Feedbacks
+from src.utils import BigQueryHelper
 
 router = APIRouter()
+big_query_helper = BigQueryHelper(key_path="src/utils/gcs_key.json", dataset_name="log")
 
 
-@router.post("/feedback", description="모든 아이템 정보를 불러옵니다. ")
+@router.post("/feedback", description="따봉 비따봉 결과 전송")
 async def create_feedback(feedback: FeedbackSchema) -> Dict:
     feedback_dict = feedback.dict(exclude_none=False)
     res = Feedbacks.create(feedback_dict)
+    # big_query_helper.insert_dict_to_table(res)
     return {"status": "success", "feedback": res}
