5be455c77d1d2abeb5e54ee4d253db1a9323ef63
ryubright <tmxkdd5790@gmail.com>
2023-02-04 23:14:09 +0900

    [Feat]: MCN inference 구현
    - 프론트가 mcn inference를 할 수 있도록 수정

--- backend/src/AI/inference.py
+++ backend/src/AI/inference.py
@@ -4,6 +4,7 @@ from src.database.models.crud_item import find_by_item_idxs
 from src.database.init_db import get_db
 from src.AI.config import MODEL_CONFIG
 from src.AI.image_processing import image_to_tensor
+from itertools import product
 
 import os
 import torch
@@ -16,6 +17,7 @@ class InferenceNewMF(object):
         self.model_path = model_config["model_path"]
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         if not os.path.exists(self.model_path):
+            os.makedirs('/'.join(self.model_path.split("/")[:-1]))
             gcs_helper = GCSHelper(
                 key_path="src/utils/gcs_key.json", bucket_name="maple_trained_model"
             )
@@ -90,19 +92,26 @@ class MCNInference:
                 blob_name="MCN/MCN_latest.pt", file_name=self.model_path
             )
 
-        self.model = MCN(
-            embed_size=model_config["embed_size"],
-            need_rep=model_config["need_rep"],
-            vocabulary=None,
-            vse_off=model_config["vse_off"],
-            pe_off=model_config["pe_off"],
-            mlp_layers=model_config["mlp_layers"],
-            conv_feats=model_config["conv_feats"],
-            pretrained=model_config["pretrained"],
-        )
+        self.item_parts = list()
         self.top_k = model_config["top_k"]
+        self.batch_size = model_config["batch_size"]
 
     async def load_model(self):
+        for part in ["Hat", "Hair", "Face", "Top", "Bottom", "Shoes", "Weapon"]:
+            db = await get_db().__anext__()
+            item_part = await find_by_item_idxs(part, db)
+            self.item_parts.append(item_part)
+
+        self.model = MCN(
+            embed_size=self.model_config["embed_size"],
+            need_rep=self.model_config["need_rep"],
+            vocabulary=None,
+            vse_off=self.model_config["vse_off"],
+            pe_off=self.model_config["pe_off"],
+            mlp_layers=self.model_config["mlp_layers"],
+            conv_feats=self.model_config["conv_feats"],
+            pretrained=self.model_config["pretrained"],
+        )
         self.model.load_state_dict(torch.load(self.model_path, map_location=self.device))
         print(self.device)
         self.model.to(self.device)
@@ -124,10 +133,92 @@ class MCNInference:
         score, _, __, ___ = self.model.forward(images)
         return float(score)
 
+    @torch.no_grad()
+    async def get_topk_codi(self, equips):
+        dummy_to_idx = {
+            0: 10093,
+            1: 10094,
+            2: 10095,
+            3: 10097,
+            4: 10098,
+            5: 10099,
+            6: 10100,
+            7: 10101
+        }
+        equips = list(equips.values())
+        pred_part_idxs = list()
+        topk_each_part = list()
+
+        for part_idx, equip in enumerate(equips):
+            part_scores = list()
+
+            if equip != -1: # Fix item
+                continue
+
+            pred_part_idxs.append(part_idx)
+            item_part = self.item_parts[part_idx]
+
+            for idx in range(0, len(item_part), self.batch_size):
+                items = item_part[idx:idx + self.batch_size]
+                input_tensors = list()
+
+                for item in items:
+                    input_items = equips[:]
+                    input_items[part_idx] = item
+
+                    input_tensor = list()
+                    for input_item_part, input_item in enumerate(input_items):
+                        if input_item == -1:
+                            input_item = dummy_to_idx[input_item_part]
+
+                        input_tensor.append(image_tensors[input_item])
+
+                    input_tensors.append(torch.stack(input_tensor))
+
+                output, _, _, _ = self.model(torch.stack(input_tensors).to(self.device))
+                for item_idx, score in zip(items, output):
+                    part_scores.append((item_idx, float(score)))
+
+            part_scores.sort(key=lambda x: x[1], reverse=True)
+            topk_each_part.append(part_scores[:self.top_k])
+
+        topk_items_each_part = [list(map(lambda x: x[0], topk_part)) for topk_part in topk_each_part]
+        topk_predict_combs = list(product(*topk_items_each_part))
+
+        equip_combs = list()
+        for topk_predict_comb in topk_predict_combs:
+            input_items = equips[:]
+            for part_idx, item_idx in zip(pred_part_idxs, topk_predict_comb):
+                input_items[part_idx] = item_idx
+
+            equip_combs.append(input_items)
+
+        codi_scores = list()
+        for idx in range(0, len(equip_combs), self.batch_size):
+            equip_comb_batch = equip_combs[idx:idx + self.batch_size]
+            input_tensors = list()
+
+            for equip_comb in equip_comb_batch:
+                input_tensor = list()
+                for item_idx in equip_comb:
+                    input_tensor.append(image_tensors[item_idx])
+
+                input_tensors.append(torch.stack(input_tensor))
+
+            output, _, _, _ = self.model(torch.stack(input_tensors).to(self.device))
+
+            for equip_comb, score in zip(equip_comb_batch, output):
+                codi_scores.append((equip_comb, float(score)))
+
+        codi_scores.sort(key=lambda x: x[1], reverse=True)
+        predict_codis = codi_scores[:self.top_k]
+        predict_codis = [list(predict_codi[0]) for predict_codi in predict_codis]
+
+        return predict_codis
+
 
 if is_load:
     newMF = InferenceNewMF(model_config=MODEL_CONFIG["newMF"])
-
     asyncio.run(newMF.load_model())
     mcn = MCNInference(model_config=MODEL_CONFIG["MCN"])
     asyncio.run(mcn.load_model())
--- backend/src/routers/inference_router.py
+++ backend/src/routers/inference_router.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from typing import Optional, Dict, List
 
 from collections import defaultdict
-from src.AI import InferenceNewMF
+from src.AI import InferenceNewMF, MCNInference
 from src.AI.inference import get_model
 
 from src.database.init_db import get_db
@@ -13,6 +13,8 @@ from src.database.models.crud_item import (
     find_by_index,
 )
 from src.routers.schemas import InferenceInput, ResultItem, InferenceResult
+
+import time
 
 
 router = APIRouter(prefix="/inference")
@@ -55,18 +57,31 @@ async def newMF_output(
     return res
 
 
-@router.get(
+@router.post(
     "/submit/MCN",
     response_model=List[InferenceResult],
-    description="codi recommendation",
+    description="mcn codi recommendation",
 )
-async def mcn_output(equips: InferenceInput):
-    """
-    get_model_output 실행~
-    :param equips:
-    :return:
-    """
-    equips = dict(equips)
-    predicts = MODELS["MCN"].inference(equips)
+async def mcn_output(
+    equips: InferenceInput,
+    model: MCNInference = Depends(get_model),
+    db: Database = Depends(get_db),
+):
+    equips_dict = equips.dict(exclude_none=False)
+    res = list()
 
-    return JSONResponse(predicts)
+    predicts = await model.get_topk_codi(equips_dict)
+    parts = ["Hat", "Hair", "Face", "Top", "Bottom", "Shoes", "Weapon"]
+
+    for predict in predicts:
+        codi_set = dict()
+        for part, item_idx in zip(parts, predict):
+            item_info = await find_by_index(item_idx, db)
+            codi_set[part] = {
+                "item_id": item_info["item_id"],
+                "name": item_info["name"],
+                "gcs_image_url": item_info["gcs_image_url"]
+            }
+        res.append(codi_set)
+
+    return res
