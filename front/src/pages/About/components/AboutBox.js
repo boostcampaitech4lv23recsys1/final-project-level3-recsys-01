1049ecb1174c71a2aa9b0df188716a72749335c4
41ow1ives <77601251@daum.net>
2023-02-04 21:03:29 +0900

    [Update]: About page Members align
    - align to center complete

--- front/src/pages/About/components/AboutBox.js
+++ front/src/pages/About/components/AboutBox.js
@@ -7,7 +7,11 @@ function AboutBox() {
       <div className="aboutBox-title"> About </div>
       <div className="aboutBox-wrap">
         <img className="aboutBox-bg" src={quests} alt="" />
-        <p className="aboutBox-text"> 안녕 </p>
+        <div className="aboutBox-text">
+          {" "}
+          이거 텍스트 정렬 맞추고 있을 바에 걍 이미지로 넣는게 정신 건강에
+          좋을듯{" "}
+        </div>
       </div>
     </div>
   );
