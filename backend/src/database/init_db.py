677e125858a8772d2422d26bccca84d2b6c53229
Jeong-Junhwan <zxxzx1515@naver.com>
2023-02-08 00:15:48 +0900

    [Update]: Mongodb client 비동기 변경
    - 기존 전역변수로 만들어 사용하던 Mongodb client를 비동기적으로 작동하도록 변경
    - Feedback은 그냥 bigquery에만 저장하도록 변경
    - 현재 Feedback에서 일치하지 않는 이슈 존재

--- backend/src/database/init_db.py
+++ backend/src/database/init_db.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+
 import os
 import certifi
 
@@ -9,16 +11,15 @@ MONGODB_URI = os.getenv("MONGODB_URI")
 # DB name
 MONGODB_DATABASE_NAME = str(os.getenv("MONGODB_DATABASE_NAME"))
 
-client = MongoClient(MONGODB_URI, tlsCAFile=ca)
-if MONGODB_URI is None:
-    print("env파일에서 URI를 읽어오지 못하고 있습니다.")
-else:
-    print("정상적으로 MongoDB 서버에 연결되었습니다.")
-db = client[MONGODB_DATABASE_NAME]
+# client = MongoClient(MONGODB_URI, tlsCAFile=ca)
+# if MONGODB_URI is None:
+#     print("env파일에서 URI를 읽어오지 못하고 있습니다.")
+# else:
+#     print("정상적으로 MongoDB 서버에 연결되었습니다.")
+# db = client[MONGODB_DATABASE_NAME]
 
 
 async def get_db():
-    try:
-        yield db
-    finally:
-        pass
+    client = AsyncIOMotorClient(MONGODB_URI)
+    db = client[MONGODB_DATABASE_NAME]
+    return db
